5c6b1750c1e5dfd3f6fb9b95cd5716dffcead922
domieyegon <domieyegon70@gmail.com>
2019-08-28 17:12:32 +0300

    progress

--- src/app/component/video-player/video-player.component.ts
+++ src/app/component/video-player/video-player.component.ts
@@ -16,13 +16,42 @@ export class VideoPlayerComponent implements OnInit {
   constructor(private videoPlayerService: VideoPlayerService) { }
 
   videoPlayer(videoUrl) {
+    // Video play instanciation
     const videoElement = document.querySelector('video');
     const player = new RxPlayer({videoElement});
-
+    // play the video file
     player.loadVideo({
       url : videoUrl,
       transport: "directfile",
       autoPlay: true
+    });
+
+    // check if the video loaded successfully
+    player.addEventListener("playerStateChange", (state) => {
+      if (state === "LOADED") {
+        console.log("the content is loaded");
+
+        const videoBitrates = player.getAvailableVideoBitrates();
+        if(videoBitrates.length) {
+          console.log("bitrates of the current video :", videoBitrates.join(", "));
+        }
+
+        // toggle play/pause when the videoElement in clicked
+        // videoElement.onclick = function() {
+        //   if(player.getPlayerState() === "PLAYING") {
+        //     player.pause();
+        //   }else {
+        //     player.play();
+        //   }
+        // }
+
+        
+      }
+    });
+
+    // check if error occured while loading the video file
+    player.addEventListener("error", (err) => {
+      console.log("the content stopped with the following error", err);
     });
   }
 
