7a3d66cc3889b296280374f4550b6a68f51a74f2
Dominic Yegon <domieyegon70@gmail.com>
2019-08-28 15:07:17 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoPlayerComponent } from './component/video-player/video-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoPlayerComponent
   ],
   imports: [
     BrowserModule,
